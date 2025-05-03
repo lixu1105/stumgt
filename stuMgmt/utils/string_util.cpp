21b1c123efc12b7a214b8a412a32ac9b1510dbf9
lixu <lixu881105@gmail.com>
2025-05-03 21:39:27 +0800

    Update string_util.cpp
    add format function

--- stuMgmt/utils/string_util.cpp
+++ stuMgmt/utils/string_util.cpp
@@ -162,3 +162,31 @@ bool string_util::contain(std::string &str, std::string str_sub)
 	if (pos == std::string::npos) return false;
 	return true;
 }
+
+std::string string_util::format(const std::string fmt, ...)
+{
+	std::string str_ret;
+	int size = ((int)fmt.size()) * 2 + 50;  // Use a rubric appropriate for your code
+	va_list ap;
+	while (true)
+	{
+		// Maximum two passes on a POSIX system...
+		str_ret.resize(size);
+		va_start(ap, fmt);
+		int n = vsnprintf((char *)str_ret.data(), size, fmt.c_str(), ap);
+		va_end(ap);
+		if (n > -1 && n < size)
+		{
+			// Everything worked
+			str_ret.resize(n);
+			return str_ret;
+		}
+
+		if (n > -1)  // Needed size returned
+			size = n + 1;   // For null char
+		else
+			size *= 2;  // Guess at a larger size (OS specific)
+	}
+
+	return str_ret;
+}
